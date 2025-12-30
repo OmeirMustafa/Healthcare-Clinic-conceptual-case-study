7809fb0e32c89244b29c636c8e3da461947e8d2f
OmeirMustafa <kaziomeirmustafa@gmail.com>
2025-12-30 10:58:42 +0600

    feat: swap entry points (index.html=casestudy, demo.html=app)

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html'),
-                caseStudy: resolve(__dirname, 'case-study.html')
+                demo: resolve(__dirname, 'demo.html')
             }
         }
     }
